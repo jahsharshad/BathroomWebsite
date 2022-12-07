a225d209313a1c75abcdd7359ae32378ca17ecf8
namver870 <64298264+namver870@users.noreply.github.com>
2022-12-07 09:23:24 -0800

    Add files via upload

---
+++ BackJS.js
@@ -0,0 +1,15 @@
+function ReportFunction() {
+document.getElementById("method").innerHTML = "";
+}
+function MapFunction() {
+document.getElementById("method").innerHTML = "";
+}
+function FindFunction() {
+document.getElementById("method").innerHTML = "";
+}
+function FeedbackFunction() {
+document.getElementById("method").innerHTML = "";
+}
+function AboutUsFunction() {
+document.getElementById("method").innerHTML = "";
+}
